3aa88ce90fb94f3f26a23d66b473e3bee3841a88
Clark <wdsgyj@gmail.com>
2020-07-10 21:50:21 +0800

    add print local ip list feature

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 )
@@ -11,6 +12,33 @@ func main() {
 		fmt.Println("Not enough arguments. For example, filesvr \":8899\" ~/Downloads")
 		os.Exit(1)
 	}
+	findLocalIpAddress()
 	http.Handle("/", http.FileServer(http.Dir(os.Args[2])))
 	fmt.Println(http.ListenAndServe(os.Args[1], nil))
+}
+
+func findLocalIpAddress() {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("--------------------------")
+	for _, inter := range interfaces {
+		if inter.Flags&net.FlagUp != 0 && inter.Flags&(net.FlagLoopback|net.FlagPointToPoint) == 0 {
+			addrs, err := inter.Addrs()
+			if err != nil {
+				continue
+			}
+
+			for i, addr := range addrs {
+				if i == 0 {
+					fmt.Println(inter.Name, inter.Flags)
+				}
+				fmt.Println(" ", addr)
+			}
+		}
+	}
+	fmt.Println("--------------------------")
 }
